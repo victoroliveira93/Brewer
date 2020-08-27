372197bfe1d155c707f2526a61667e606a866048
victorangra <victor_oliveira93@outlook.com>
2020-08-27 13:19:28 -0300

    Aula 12.2

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,7 +1,5 @@
 package com.algaworks.brewer.controller;
 
-import java.util.Optional;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,34 +8,43 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
-import com.algaworks.brewer.repository.Cervejas;
+import com.algaworks.brewer.model.Origem;
+import com.algaworks.brewer.model.Sabor;
+import com.algaworks.brewer.repository.Estilos;
+import com.algaworks.brewer.service.CadastroCervejaService;
 
 @Controller
 public class CervejasController {
 	
 	@Autowired
-	private Cervejas cervejas;
+	private Estilos estilos;
+	
+	@Autowired
+	private CadastroCervejaService cadastroCervejaService;
 	
 	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
-		Optional<Cerveja> cervejaOptional = cervejas.findBySku("AA1111"); 
-		System.out.println(cervejaOptional.isPresent());
-		return "cerveja/CadastroCerveja";
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		return mv;
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(cerveja);
 		}
 		
 		//Salvando no banco de dados...
+		cadastroCervejaService.salvar(cerveja);
 		attributes.addFlashAttribute("mensagem", "Cerveja cadastrada com sucesso");
-		System.out.println(">>>>sky " + cerveja.getSku());
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 
 }
